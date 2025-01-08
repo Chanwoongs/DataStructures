d0a77520cc0075f62a8a5c2fafaec79a84ec7076
Chanwoong Bang <tkfkdcksdnd@daum.net>
2025-01-08 15:27:54 +0900

    feat: template stack

---
+++ DataStructrue/7.TemplateStack/Stack.h
@@ -0,0 +1,67 @@
+﻿#pragma once
+
+#include <iostream>
+
+template<typename T, int size = 10>
+class Stack
+{
+public:
+    Stack()
+        : count(0)
+    {
+        memset(data, 0, sizeof(T) * size);
+    }
+
+    // 초기화
+    void Clear()
+    {
+        count = 0;
+    }
+
+    // 스택에 저장된 요소의 수 반환
+    inline int Count() const { return count; }
+
+    // 스택이 비었는지 확인하는 함수
+    inline bool IsEmpty() const { return count == 0; }
+    
+    // 스택이 가득 찼는지 확인하는 함수
+    inline bool IsFull() const { return count == size; }
+
+    // 데이터 저장 함수
+    bool Push(const T& value)
+    {
+        if (IsFull())
+        {
+            std::cout << "스택이 가득차 Push 실패\n";
+            return false;
+        }
+
+        // 저장 및 카운터 증가
+        data[count] = value;
+        ++count;
+        return true;
+    }
+
+    // 데이터 반환 (삭제) 함수
+    bool Pop(T& outValue)
+    {
+        if (IsEmpty())
+        {
+            std::cout << "스택이 비어있어 Pop 실패\n";
+            return false;
+        }
+
+        // 카운터 감소 후 데이터 반환
+        --count;
+        outValue = data[count];
+        return true;
+    }
+
+private:
+    // 데이터 저장 배열
+    T data[size];
+
+    // 스택에 저장된 요소의 수(top/head)
+    int count;
+
+};
---
+++ DataStructrue/7.TemplateStack/main.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+
+#include "Stack.h"
+
+int main()
+{
+    // 스택 객체
+    Stack<float, 20> stack;
+
+    if (stack.Push(140.0f))
+    {
+        std::cout << "첫 번째 게임 종료 - 경험치 140\n";
+    }
+    if (stack.Push(180.0f))
+    {
+        std::cout << "두 번째 게임 종료 - 경험치 180\n";
+    }
+    if (stack.Push(200.0f))
+    {
+        std::cout << "세 번째 게임 종료 - 경험치 200\n";
+    }
+
+    while (!stack.IsEmpty())
+    {
+        float value;
+        if (stack.Pop(value))
+        {
+            std::cout << "현재 경험치: " << value << '\n';
+        }
+    }
+}
