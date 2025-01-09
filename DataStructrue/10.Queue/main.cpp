30c10a41c06a2e7bb5f0bc7da489b4ab0ee5642e
Chanwoong Bang <tkfkdcksdnd@daum.net>
2025-01-09 16:07:34 +0900

    feat: circular queue

---
+++ DataStructrue/10.Queue/main.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+
+#include "Queue.h"
+
+int main()
+{
+    Queue queue;
+
+    for (int i = 0; i < 10; i++)
+    {
+        queue.Enqueue(i + 10);
+    }
+    
+    queue.Print();
+
+    for (int i = 0; i < 10; i++)
+    {
+        int value;
+        queue.Dequeue(value);
+    }
+    std::cout << '\n';
+
+    queue.Print();
+}
