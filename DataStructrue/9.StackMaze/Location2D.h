d19cf7c8f06889fe21f8cd717854fc37003c03da
Chanwoong Bang <tkfkdcksdnd@daum.net>
2025-01-09 14:48:29 +0900

    feat: stack을 활용한 DFS 탐색

---
+++ DataStructrue/9.StackMaze/Location2D.h
@@ -0,0 +1,15 @@
+﻿#pragma once
+
+class Location2D
+{
+public:
+    Location2D() {}
+    Location2D(int row, int column)
+        : row(row), column(column)
+    {
+    }
+
+public:
+    int row = 0;
+    int column = 0;
+};
