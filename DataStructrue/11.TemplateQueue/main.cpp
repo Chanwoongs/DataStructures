91b03521007226d9600e3fab291b1a9391fa1f44
Chanwoong Bang <tkfkdcksdnd@daum.net>
2025-01-10 10:52:40 +0900

    Template Queue

---
+++ DataStructrue/11.TemplateQueue/main.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+
+#include "Queue.h"
+
+int main()
+{
+    Queue<int> queue;
+
+    queue.Enqueue(10);
+    queue.Enqueue(20);
+    queue.Enqueue(30);
+    queue.Enqueue(40);
+    queue.Enqueue(50);
+
+    std::cout << "----- 데이터 추가 후 출력 -----\n";
+    queue.Print();
+
+    int outValue = 0;
+    if (queue.Dequeue(outValue))
+    {
+        std::cout << "큐에서 출력된 값: " << outValue << '\n';
+    } 
+    outValue = 0;
+    if (queue.Dequeue(outValue))
+    {
+        std::cout << "큐에서 출력된 값: " << outValue << '\n';
+    }
+    outValue = 0;
+    if (queue.Dequeue(outValue))
+    {
+        std::cout << "큐에서 출력된 값: " << outValue << '\n';
+    }
+
+    std::cout << "----- 데이터 삭제 후 출력 -----\n";
+    queue.Print();
+}
