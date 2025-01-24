6361f8205c07bceee598b7f6a6f9d85142341aeb
Chanwoong Bang <tkfkdcksdnd@daum.net>
2025-01-24 15:42:24 +0900

    이진 탐색 트리

---
+++ DataStructrue/16.BinarySearchTreee/Node.h
@@ -0,0 +1,40 @@
+﻿#pragma once
+
+template<typename T>
+class Node
+{
+    // 프렌드 선언
+    template<typename T>
+    friend class BinarySearchTree;
+
+public:
+    Node(const T& data)
+        : data(data), parent(nullptr), left(nullptr), right(nullptr)
+    {
+
+    }
+    Node(const T& data, Node<T>* parent)
+        : data(data), parent(parent), left(nullptr), right(nullptr)
+    {
+
+    }
+    ~Node()
+    {
+        parent = nullptr;
+        left = nullptr;
+        right = nullptr;
+    }
+
+private:
+    // 변수
+    T data;
+
+    // 부모 노드
+    Node<T>* parent;
+
+    // 왼쪽 자손
+    Node<T>* left;
+
+    // 오른쪽 자손
+    Node<T>* right;
+};
---
+++ DataStructrue/16.BinarySearchTreee/main.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+
+#include "BinarySearchTree.h"
+
+int main()
+{
+    BinarySearchTree<int> tree;
+
+    tree.InsertNode(20);
+    tree.InsertNode(20);
+    tree.InsertNode(50);
+    tree.InsertNode(50);
+    tree.InsertNode(70);
+    tree.InsertNode(80);
+    tree.InsertNode(100);
+
+    int searchData = 50;
+    if (tree.SearchNode(searchData))
+    {
+        std::cout << searchData << " 검색 성공\n";
+    }
+    else
+    {
+        std::cout << searchData << " 검색 실패\n";
+    }
+
+    tree.DeleteNode(30);
+    tree.InsertNode(30);
+
+    tree.InorderTraverse();
+}
